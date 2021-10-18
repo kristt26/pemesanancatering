c51e9465cdd133f4e3e0cf9c62d88eb976d4cc07
kristt26 <kristt26@gmail.com>
2021-10-18 09:14:54 +0900

    as

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        $dataa['content'] = 5;
-        return view('layout/backend/welcome', $dataa);
+        $data['title'] = ["title" => "Home", "sub" => ""];
+        $data['content'] = "";
+        $data['sidebar'] = view("layout/backend/sidebar", $data['title']);
+        return view('layout/backend/welcome', $data);
     }
 }
